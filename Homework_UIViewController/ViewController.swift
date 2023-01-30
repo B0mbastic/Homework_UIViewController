1635ee938ac2322af6e84246d614def2085aed0e
B0mbastic <kovbasin@gmai.com>
2023-01-31 00:01:50 +0800

    Initial Commit

---
+++ Homework_UIViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_UIViewController
+//
+//  Created by Александр Ковбасин on 31.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
