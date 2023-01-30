27599014620c156e4bb4b335200219cdc57b3097
B0mbastic <kovbasin@gmai.com>
2023-01-31 00:58:15 +0800

    added print "test2"

--- Homework_UIViewController/ViewController.swift
+++ Homework_UIViewController/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("test")
+        print("test2")
         // Do any additional setup after loading the view.
     }
 
